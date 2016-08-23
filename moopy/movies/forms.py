bc54c524b2d56433ac7ab8db2d7aaa30d51ec192
qrizan <qrizan@gmail.com>
2016-08-23 12:04:17 +0700

    markdown preview

--- moopy/movies/forms.py
+++ moopy/movies/forms.py
@@ -1,8 +1,12 @@
 from django import forms
+from pagedown.widgets import PagedownWidget
 from .models import Movie, Genre
 
 
 class MovieForm(forms.ModelForm):
+    description = forms.CharField(widget=PagedownWidget(show_preview=False))
+    publish = forms.DateField(widget=forms.SelectDateWidget)
+
     class Meta:
         model = Movie
         fields = [
--- moopy/static/js/base.js
+++ moopy/static/js/base.js
@@ -1,1 +1,41 @@
+$(document).ready(function(){
+    $(".content-markdown").each(function(){
+        var content = $(this).text()
+        var contentMarkdown = marked(content)
+        $(this).html(contentMarkdown)
+    })
+
+    $(".content-markdown img").each(function(){
+            $(this).addClass("ui fluid image")
+    })
+
+    var titleForm = $('#id_title');
+    setTitle(titleForm.val())
+
+    var descriptionForm = $('#id_description');
+    setDescription(descriptionForm.val())
+
+    titleForm.keyup(function() {
+        var titleNew = $(this).val()
+        setTitle(titleNew)
+    })
+
+    descriptionForm.keyup(function() {
+        var descriptionNew = $(this).val()
+        setDescription(descriptionNew)
+    })
+
+    function setDescription(value){
+        var markedDescription = marked(value)
+        $("#preview-description").html(markedDescription)
+        $("#preview-description img").each(function(){
+            $(this).addClass("ui fluid image")
+        })
+    }
+
+    function setTitle(value){
+        $("#preview-title").text(value)
+    }
+
+})
 $('.ui.embed').embed();
